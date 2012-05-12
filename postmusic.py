0cd49ec5c0f1286a7914514584949e5e9f4617d1
nyon <clarinet@gmail.com>
2012-05-12 23:25:59 +0900

    postmusic.py

--- postmusic.py
+++ postmusic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import sys
 from urllib import urlencode
@@ -13,6 +13,6 @@ client = Client(Consumer(consumer_key,consumer_secret), Token(user_key,user_secr
 argvs = sys.argv
 for argv in argvs:
   print argv
+message = "playing " + argvs[2] + " from " + argvs[3] + "(" + argvs[1] + ")" + " #実験中"  + " ＠ " + argvs[4] + "回目"
+client.request('http://api.twitter.com/1/statuses/update.xml', 'POST', urlencode({'status':message}))
 
-message = "playing " + argvs[2] + " from " + argvs[3] + "(" + argvs[1] + ")"  # + " ＠ " + argvs[4] + " 回目 "
-client.request("http://api.twitter.com/1/statuses/update.xml", "POST", urlencode({'status':message}))
